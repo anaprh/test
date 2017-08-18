2250794f3bf0f00d63c94dd5c47c01c4c08e8b4e
Anastasiya <anastasiyaprh@MacBook-Pro-Anastasiya.local>
2017-08-18 21:31:38 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,20 @@
+var sampleArray = [0,0,7];
+
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+     
+    // Your code should make newArray equal to an array that has the same
+    // values as _array, but the last number has increased by one.
+    
+    // For example:
+    // _array = [1, 2, 3];
+    // turns into:
+    // newArray = [1, 2, 4];
+    
+    // Your code goes in here!
+    newArray = _array.push("ugh");
+    
+    // Don't delete this line!
+    return newArray;
+};
+console.log(incrementLastArrayElement(sampleArray));
